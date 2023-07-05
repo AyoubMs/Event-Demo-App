c4101a0d521a15532922eda348dd73f1b484e141
AyoubMS <chahayb563@gmail.com>
2023-07-05 12:48:17 +0200

    add app

---
+++ EventDemo/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CollegeClassModel history = new CollegeClassModel("History 101", 3);
+            CollegeClassModel math = new CollegeClassModel("Calculus 201", 2);
+
+            history.EnrollmentFull += CollegeClass_EnrollmentFull;
+
+            history.SignUpStudent("Tim Corey").PrintToConsole();
+            history.SignUpStudent("Sue Storm").PrintToConsole();
+            history.SignUpStudent("Joe Smith").PrintToConsole();
+            history.SignUpStudent("Mary Jones").PrintToConsole();
+            history.SignUpStudent("Sandy Patty").PrintToConsole();
+            Console.WriteLine();
+
+            math.EnrollmentFull += CollegeClass_EnrollmentFull;
+
+            math.SignUpStudent("Sue Storm").PrintToConsole();
+            math.SignUpStudent("Joe Smith").PrintToConsole();
+            math.SignUpStudent("Mary Jones").PrintToConsole();
+
+            Console.ReadLine();
+        }
+
+        private static void CollegeClass_EnrollmentFull(object sender, string e)
+        {
+            CollegeClassModel model = (CollegeClassModel)sender;
+
+            Console.WriteLine();
+            Console.WriteLine($"{model.CourseTitle}: Full");
+            Console.WriteLine();
+        }
+    }
+
+    public static class ConsoleHelpers
+    {
+        public static void PrintToConsole(this string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    public class CollegeClassModel
+    {
+        public event EventHandler<string> EnrollmentFull;
+
+        private List<string> enrolledStudents = new List<string>();
+        private List<string> waitingList = new List<string>();
+
+        public string CourseTitle { get; private set; }
+        public int MaximumStudents { get; private set; }
+
+        public CollegeClassModel(string title, int maximumStudents)
+        {
+            CourseTitle = title;
+            MaximumStudents = maximumStudents;
+        }
+
+        public string SignUpStudent(string studentName)
+        {
+            string output = "";
+
+            if (enrolledStudents.Count < MaximumStudents)
+            {
+                enrolledStudents.Add(studentName);
+                output = $"{studentName} was enrolled in {CourseTitle}";
+                // Check to see if we are maxed out
+                if (enrolledStudents.Count == MaximumStudents)
+                {
+                    EnrollmentFull?.Invoke(this, $"{CourseTitle} enrollment is now full.");
+                }
+            }
+            else
+            {
+                waitingList.Add(studentName);
+                output = $"{studentName} was added to the wait list in {CourseTitle}";
+            }
+
+            return output;
+        }
+    }
+}
